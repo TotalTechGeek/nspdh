beef40d6dccd0c0bd5aa0435d8b1bde85af6ec14
TotalTechGeek <TotalTechGeek@users.noreply.github.com>
2018-02-24 05:25:55 -0600

    Attempt to Accelerate Build Process (First time)
    Since dave currently lacks filtered extraction, I manually wrote some accelerators.
    
    These will take advantage of either the 7za or unzip utilities.
    
    I had to remove the -j flag due to a weird bug where it hung.

--- init.js
+++ init.js
@@ -12,18 +12,43 @@ Resource("http://lionet.info/soft/asn1c-0.9.28.tar.gz", "asn1c.tar.gz", function
 // Boost
 Resource("https://dl.bintray.com/boostorg/release/1.65.1/source/boost_1_65_1.zip", "boost.zip", function(f)
 {
-    extract(f);
+    if(detect("unzip"))
+    {
+        // For accelerated extraction.
+        var str = '/*'
+        for(var i = 0; i < 6; i++)
+        {
+            sys("unzip boost.zip boost_1_65_1/boost" + str)
+            str += '/*'
+            waitAll()
+        }
+    }
+    else if(detect("7za") || detect("7z") || detect("p7zip"))
+    {
+        // For accelerated extraction.
+        var prog = "7za";
+        if(detect("7z")) prog = "7z"
+        if(detect("p7zip")) prog = "p7zip"
+
+        sys(prog + " x boost.zip boost_1_65_1/boost")
+    }
+    else
+    {
+        say("This extraction might be slow.")
+        extract(f);
+    } 
 });
 
 function make_nspdh()
 {
+    say("Building program.")
     var make = "make";
     if(detect("mingw32-make"))
     {
         make = "mingw32-make";
     }
 
-    sys(make + " -j", function()
+    sys(make, function()
     {
         say("nspdh built.");       
         if(detect("upx")) 
@@ -34,4 +59,5 @@ function make_nspdh()
     });
 }
 
+waitAll()
 make_nspdh()
