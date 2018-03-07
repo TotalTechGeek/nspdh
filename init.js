b69e768903117d81b26013de3627c07355ba0184
TotalTechGeek <TotalTechGeek@users.noreply.github.com>
2018-03-06 22:01:20 -0600

    Added Visual Studio Support

--- init.js
+++ init.js
@@ -5,7 +5,6 @@ if(isMac())
     say("")
 }
 
-
 // Asn1c
 Resource("http://lionet.info/soft/asn1c-0.9.28.tar.gz", "asn1c.tar.gz", function (f) 
 { 
@@ -23,33 +22,74 @@ Resource("https://www.cryptopp.com/cryptopp600.zip", "cryptopp.zip", function(f)
     extract2(f, "cryptopp")
 })
 
-// For cryptopp
-if(isWindows())
+
+function make_cryptopp()
 {
-    var make = "make"
-    if(detect("mingw32-make")) make = "mingw32-make"
-    sys("cd cryptopp & " + make + " libcryptopp.a")
+    // For cryptopp
+    if(isWindows())
+    {   
+        // This will be modified later on to allow you to pick which build you want to execute.
+        if(detect("msbuild"))
+        {
+            sys("cd cryptopp & msbuild cryptlib.vcxproj /p:Configuration=Release /p:Platform=x64")
+        }
+        else
+        {
+            var make = "make"
+            if(detect("mingw32-make")) make = "mingw32-make"
+            sys("cd cryptopp & " + make + " libcryptopp.a")
+        }
+    }
+    else
+    {
+        sys("cd cryptopp; make libcryptopp.a")
+    }
 }
-else
+
+make_cryptopp()
+
+function nspdhOut()
 {
-    sys("cd cryptopp; make libcryptopp.a")
+    say("nspdh built.");       
+    if(detect("upx")) 
+        sys("upx nspdh.exe --lzma")
+}
+
+function make_asn1c()
+{
+    if(detect("msbuild"))
+    {
+        sys('cd asn1c-0.9.28/asn1c & cl /I"../libasn1compiler" /I"../libasn1print" /I"../libasn1parser" /I"../libasn1fix" /I"../skeletons" /DBufferMode /c enber.c')
+        waitAll()
+    }
+    else
+    {
+        // already executed in make_nspdh
+    }
 }
 
 function make_nspdh()
 {
     say("Building program.")
-    var make = "make";
-    if(detect("mingw32-make"))
+
+     // This will be modified later on to allow you to pick which build you want to execute.
+    if(detect("msbuild"))
     {
-        make = "mingw32-make";
+        make_asn1c()
+        sys("cl /DLINKASN1C /DREQUIRE_XML_EXPORT src/*.cpp /openmp /Ox cryptopp/x64/Output/Release/cryptlib.lib asn1c-0.9.28/asn1c/enber.obj /DForce2011 /Fenspdh", nspdhOut)
+    }
+    else
+    {
+        var make = "make";
+        if(detect("mingw32-make"))
+        {
+            make = "mingw32-make";
+        }
+
+        sys(make, nspdhOut);
     }
 
-    sys(make, function()
-    {
-        say("nspdh built.");       
-        if(detect("upx")) 
-             sys("upx nspdh.exe --lzma")
-    });
+    
 }
 
 waitAll()
